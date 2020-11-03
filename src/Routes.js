4ffe89c02a842c259c526c2a51bce7847319ae65
Arijit Mondal <arijitm@capitalnumbers.com>
2020-11-03 14:41:21 +0530

    Cg

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
 
 export default function Routes() {
   return (
@@ -11,6 +12,9 @@ export default function Routes() {
       </Route>
       <Route>
         <NotFound />
+      </Route>
+      <Route exact path="/login">
+        <Login />
       </Route>
     </Switch>
   );
