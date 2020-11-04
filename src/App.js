1e0faef111396a66673e0d1b979da449bcae1a31
Arijit Mondal <arijitm@capitalnumbers.com>
2020-11-04 17:42:42 +0530

    Work on login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
 import { LinkContainer } from "react-router-bootstrap";
 import { AppContext } from "./libs/contextLib";
-
+import { Auth } from "aws-amplify";
 
 function App() {
-  function handleLogout() {
-    userHasAuthenticated(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch (e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+
+    setIsAuthenticating(false);
   }
 
-  const [isAuthenticated, userHasAuthenticated] = useState(false);
-  
+  async function handleLogout() {
+    await Auth.signOut();
+
+    userHasAuthenticated(false);
+    history.push("/login");
+  }
+
+
   return (
+    !isAuthenticating &&
     <div className="App container">
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
@@ -39,7 +64,9 @@ function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+      <AppContext.Provider
+        value={{ isAuthenticated, userHasAuthenticated }}
+      >
         <Routes />
       </AppContext.Provider>
     </div>
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -3,26 +3,29 @@ import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import "./Login.css";
 import { Auth } from "aws-amplify";
 import { useAppContext } from "../libs/contextLib";
+import { useHistory } from "react-router-dom";
 
 export default function Login() {
   const { userHasAuthenticated } = useAppContext();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const history = useHistory();
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
   }
 
-async function handleSubmit(event) {
-  event.preventDefault();
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-  try {
-    await Auth.signIn(email, password);
-    userHasAuthenticated(true);
-  } catch (e) {
-    alert(e.message);
+    try {
+      await Auth.signIn(email, password);
+      userHasAuthenticated(true);
+      history.push("/");
+    } catch (e) {
+      alert(e.message);
+    }
   }
-}
 
   return (
     <div className="Login">
